a0db6371f636dff02256db46d87d70fb09348963
Trypoxylus <samuraisho0123@gmail.com>
2023-11-29 17:45:21 +0900

    add useState

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import './App.css';
 import './style.css';
 
 export const Todo = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(["TODO1", "TODO2"]);
+  const [completeTodos, setCompleteTodos] = useState(["TODO1", "TODO2"]);
   return (
     <>
       <div className="input-area">
@@ -11,37 +14,33 @@ export const Todo = () => {
       <div className="incomplete-area">
         <p className='title'>INCOMPLETE TODO</p>
         <ul>
-          <li>
-            <div className='list-row'>
-              <p className='todo-item'>TODOです</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
-          <li>
-            <div className='list-row'>
-              <p className='todo-item'>TODOです</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
+          {incompleteTodos.map((todo) => {
+            return (
+              <li key={todo}>
+                <div className='list-row'>
+                  <p className='todo-item'>{todo}</p>
+                  <button>done</button>
+                  <button>delete</button>
+                </div>
+              </li>
+            )
+          })}
+          
         </ul>
       </div>
       <div className='complete-area'>
       <p className='title'>COMPLETE TODO</p>
         <ul>
-          <li>
-            <div className='list-row'>
-              <p className='todo-item'>TODOです</p>
-              <button>back</button>
-            </div>
-          </li>
-          <li>
-            <div className='list-row'>
-              <p className='todo-item'>TODOです</p>
-              <button>back</button>
-            </div>
-          </li>
+          {completeTodos.map((todo) => {
+            return (
+              <li>
+                <div className='list-row'>
+                  <p className='todo-item'>{todo}</p>
+                  <button>back</button>
+                </div>
+              </li>
+            )
+          })}
         </ul>
       </div>
     </>
